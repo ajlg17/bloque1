375baea5dff0473dde43d0da6b9ac90c22e2934a
Andres Lopez <andresjlg17@gmail.com>
2022-05-06 19:18:14 +0200

    he modificado el Main añadiendo una persona y usando el metodo devolverRandom

--- bloque1/src/Controlador/Main.java
+++ bloque1/src/Controlador/Main.java
@@ -1,11 +1,16 @@
 package Controlador;
 
 import bloque1.*;
+import static bloque1.Bloque1.devuelveRandom;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Persona persona1 = new Persona("Victor",20);
+        
+        System.out.println(devuelveRandom(2, 17));
+        
+        Bloque1.imprimeDatoPersona(persona1);
     }
 
 }
