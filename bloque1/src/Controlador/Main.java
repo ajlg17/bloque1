9efe9e540e44ba8bfe14df14cc04bb676fbd057a
Victor Vera <victorveracoronel@live.com>
2022-05-06 19:19:38 +0200

    He cambiado un poquito el main para joder a Andres

--- bloque1/src/Controlador/Main.java
+++ bloque1/src/Controlador/Main.java
@@ -5,7 +5,10 @@ import bloque1.*;
 public class Main {
 
     public static void main(String[] args) {
-
+        Persona personaMayorEdad = new Persona("Manolo",25);   
+        Bloque1.imprimeDatoPersona(personaMayorEdad);
+        
+        System.out.println(Bloque1.devuelveRandom(50, 25));
     }
 
 }
